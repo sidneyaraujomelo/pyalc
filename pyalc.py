1d12dcbab73f90ce2dde30bb1891545c357f3654
Sidney Melo <sidneyaraujomelo@gmail.com>
2019-11-10 00:20:30 -0300

    Complete pivoting

--- pyalc.py
+++ pyalc.py
@@ -11,6 +11,7 @@ def partial_pivoting(matrix_A, vector_b):
         #print(sub_matrix)
         max_col_id = np.argmax(sub_matrix, axis=0)[0]
         if max_col_id != 0:
+            print(f"Exchange rows {p+max_col_id} and {p}")
             matrix_A[[p+max_col_id, p]] = matrix_A[[p, p+max_col_id]]
             vector_b[[p+max_col_id, p]] = vector_b[[p, p+max_col_id]]
         print(matrix_A)
@@ -32,12 +33,40 @@ def partial_pivoting_with_scale(matrix_A, vector_b):
         print(f"Scaled columns: {scaled_col}")
         max_col_id = np.argmax(scaled_col)
         if max_col_id != 0:
-            print(f"Exchange {p+max_col_id} and {p}")
+            print(f"Exchange rows {p+max_col_id} and {p}")
             matrix_A[[p+max_col_id, p]] = matrix_A[[p, p+max_col_id]]
             vector_b[[p+max_col_id, p]] = vector_b[[p, p+max_col_id]]
         print(matrix_A)
         print(vector_b)
     return matrix_A, vector_b
+
+
+def complete_pivoting(matrix_A, vector_b):
+    """ Apply complete pivoting to the system """
+    print("=== Complete Pivoting ===")
+    reorder = np.arange(vector_b.shape[0])
+    print(f"X order: {reorder}")
+    for p in range(matrix_A.shape[0]-1):
+        print(f"== step {p}:")
+        sub_matrix = matrix_A[p:, p:]
+        #print(sub_matrix)
+        max_val_index = np.unravel_index(np.argmax(np.abs(sub_matrix)), sub_matrix.shape)
+        print(max_val_index)
+        max_row_id = max_val_index[0]
+        if max_row_id != 0:
+            print(f"Exchange rows {p+max_row_id} and {p}")
+            matrix_A[[p+max_row_id, p]] = matrix_A[[p, p+max_row_id]]
+            vector_b[[p+max_row_id, p]] = vector_b[[p, p+max_row_id]]
+        max_col_id = max_val_index[1]
+        if max_col_id != 0:
+            print(f"Exchange columns {p+max_col_id} and {p}")
+            matrix_A[:, [p+max_col_id, p]] = matrix_A[:, [p, p+max_col_id]]
+            reorder[[p+max_col_id, p]] = reorder[[p, p+max_col_id]]
+        print(matrix_A)
+        print(vector_b)
+        print(reorder)
+        #input("...")
+    return matrix_A, vector_b, reorder
 
 
 def regressive_substitution(matrix_A, vector_b):
@@ -56,7 +85,7 @@ def regressive_substitution(matrix_A, vector_b):
         #print(post_a)
         num = vector_b[k] - np.sum(post_x*post_a)
         x[k] = num / dem
-    print(f"Final solution: {x}")
+    print(f"Solution: {x}")
     return x
 
 
@@ -92,17 +121,22 @@ def main():
     b_file = args.b
     pivot = args.p if args.p else None
 
-    matrix_A = np.loadtxt(A_file)
-    vector_b = np.loadtxt(b_file)
-    print(matrix_A)
-    print(vector_b)
+    o_matrix_A = np.loadtxt(A_file)
+    o_vector_b = np.loadtxt(b_file)
+    print(o_matrix_A)
+    print(o_matrix_A)
 
     if pivot == "partial":
-        matrix_A, vector_b = partial_pivoting(matrix_A, vector_b)
+        matrix_A, vector_b = partial_pivoting(o_matrix_A, o_vector_b)
     elif pivot == "partial_scale":
-        matrix_A, vector_b = partial_pivoting_with_scale(matrix_A, vector_b)
+        matrix_A, vector_b = partial_pivoting_with_scale(o_matrix_A, o_vector_b)
+    elif pivot == "complete":
+        matrix_A, vector_b, reorder = complete_pivoting(o_matrix_A, o_vector_b)
 
-    gaussian_elimination(matrix_A, vector_b)
+    x, matrix_A, vector_b = gaussian_elimination(matrix_A, vector_b)
+
+    if pivot == "complete":
+        print(f"Reordered solution: {x[np.argsort(reorder)]}")
 
 if __name__ == "__main__":
     main()
